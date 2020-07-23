9a0d6720f485690791e4435acca1fe4449d44c68
Leon <q593648942@outlook.com>
2020-07-23 10:30:57 +0800

    Text field suggestion

--- src/main/java/top/leonx/territory/client/screen/TerritoryPermissionScreen.java
+++ src/main/java/top/leonx/territory/client/screen/TerritoryPermissionScreen.java
@@ -10,6 +10,7 @@ import top.leonx.territory.container.TerritoryContainer;
 import top.leonx.territory.data.PermissionFlag;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.function.Consumer;
 
@@ -22,6 +23,7 @@ public class TerritoryPermissionScreen extends AbstractScreenPage<TerritoryConta
     private CheckboxButton enterCheckbox;
     private CheckboxButton breakCheckbox;
     private CheckboxButton interactCheckbox;
+    private GuiButtonExt addPlayerBtn;
     public TerritoryPermissionScreen(TerritoryContainer container,
                                      ContainerScreen<TerritoryContainer> parent, Consumer<Integer> changePage) {
         super(container,parent,changePage);
@@ -31,6 +33,7 @@ public class TerritoryPermissionScreen extends AbstractScreenPage<TerritoryConta
         final int halfW = width / 2;
         //noinspection unused
         final int halfH = height / 2;
+
         this.addButton(new GuiButtonExt(halfW+40, parent.getGuiTop()+parent.getYSize()-30, 70, 20, "BACK",
                 $ -> NavigateTo(0)
         ));
@@ -43,63 +46,38 @@ public class TerritoryPermissionScreen extends AbstractScreenPage<TerritoryConta
 
         search=new TextFieldWidget(font, halfW-110, parent.getGuiTop()+8, 100,16,"search");
         addTextField=new TextFieldWidget(font, halfW-110, parent.getGuiTop()+parent.getYSize()-24, 80,16,"NAME");
-
-        this.addButton(new GuiButtonExt(halfW-110+80,parent.getGuiTop()+parent.getYSize()-24,20,16,"+",t-> addNewPlayer()));
+        addPlayerBtn=new GuiButtonExt(halfW-110+80,parent.getGuiTop()+parent.getYSize()-24,20,16,"+",
+                t-> addNewPlayer());
         breakCheckbox=new CheckboxButton(halfW,parent.getGuiTop()+8,50,20,"Break",true);
-        this.addButton(breakCheckbox);
         interactCheckbox =new CheckboxButton(halfW,parent.getGuiTop()+36,50,20,"Right Click",true);
-        this.addButton(interactCheckbox);
+
+        addTextField.setSuggestion("ADD PLAYER");
+        search.setSuggestion("SEARCH");
+        addPlayerBtn.active=false;
 
         this.children.add(playerList);
         this.children.add(addTextField);
-        this.children.add(playerList);
         this.children.add(search);
-
-    }
-
-    private void addNewPlayer() {
-        String name=addTextField.getText();
-        UUID uuid=null;
-        for (Map.Entry<UUID,String> entry:UsernameCache.getMap().entrySet())
-        {
-            if(entry.getValue().equals(name))
-            {
-                uuid=entry.getKey();
-                break;
-            }
-        }
-
-        if(uuid==null)
-        {
-            return;
-        }
-        addTextField.setText("");
-        if(container.territoryInfo.permissions.containsKey(uuid)){
-            UUID finalUuid = uuid;
-            //noinspection OptionalGetWithoutIsPresent
-            playerList.setSelected(playerList.children().stream().filter(t->t.getUUID().equals(finalUuid)).findFirst().get());
-        }
-        else{
-            container.territoryInfo.permissions.put(uuid,new PermissionFlag());
-            playerList.children().add(playerList.new PlayerEntry(uuid,name,playerList));
-        }
-        //container.detectAndSendChanges();
+        this.addButton(addPlayerBtn);
+        this.addButton(breakCheckbox);
+        this.addButton(interactCheckbox);
     }
 
     @Override
     public void renderInternal(int mouseX, int mouseY, float partialTicks) {
         playerList.render(mouseX,mouseY,partialTicks);
         search.render(mouseX,mouseY,partialTicks);
-        if(!search.isFocused() && search.getText().length()>0)
-        {
-            font.drawString(search.getMessage(),search.x+4,search.y+4,0xCCCCCCCC);
-        }
+        search.renderButton(mouseX,mouseY,partialTicks);
+//        if(!search.isFocused() && search.getText().length()==0)
+//        {
+//            font.drawString(search.getMessage(),search.x+4,search.y+4,0xCCCCCCCC);
+//        }
         addTextField.render(mouseX,mouseY,partialTicks);
-
-        if(!addTextField.isFocused() && addTextField.getText().length()>0)
-        {
-            font.drawString(addTextField.getMessage(),addTextField.x+4,addTextField.y+4,0xCCCCCCCC);
-        }
+        addTextField.renderButton(mouseX,mouseY,partialTicks);
+//        if(!addTextField.isFocused() && addTextField.getText().length()==0)
+//        {
+//            font.drawString(addTextField.getMessage(),addTextField.x+4,addTextField.y+4,0xCCCCCCCC);
+//        }
     }
 
     @Override
@@ -121,6 +99,8 @@ public class TerritoryPermissionScreen extends AbstractScreenPage<TerritoryConta
             );
             lastTickSearch=search.getText();
         }
+
+
         if(playerList.getSelected()== null) return;
         if(lastPlayerEntry!=playerList.getSelected())
         {
@@ -147,5 +127,74 @@ public class TerritoryPermissionScreen extends AbstractScreenPage<TerritoryConta
             lastTickBreakChecked=breakCheckbox.isChecked();
             lastTickInteracteChecked= interactCheckbox.isChecked();
         }
+    }
+
+    @Override
+    public boolean keyPressed(int p_keyPressed_1_, int p_keyPressed_2_, int p_keyPressed_3_) {
+        boolean result= super.keyPressed(p_keyPressed_1_, p_keyPressed_2_, p_keyPressed_3_);
+        updateSuggestion();
+        return result;
+    }
+
+    @Override
+    public boolean charTyped(char p_charTyped_1_, int p_charTyped_2_) {
+        boolean r=super.charTyped(p_charTyped_1_,p_charTyped_2_);
+        updateSuggestion();
+        return r;
+    }
+    private String addTextFieldSuggestion; //why is there no direct way to get;
+    private void addNewPlayer() {
+        String name=addTextField.getText()+addTextFieldSuggestion;
+        UUID uuid=null;
+        for (Map.Entry<UUID,String> entry:UsernameCache.getMap().entrySet())
+        {
+            if(entry.getValue().equals(name))
+            {
+                uuid=entry.getKey();
+                break;
+            }
+        }
+
+        if(uuid==null)
+        {
+            return;
+        }
+        addTextField.setText("");
+        updateSuggestion();
+        if(container.territoryInfo.permissions.containsKey(uuid)){
+            UUID finalUuid = uuid;
+            //noinspection OptionalGetWithoutIsPresent
+            playerList.setSelected(playerList.children().stream().filter(t->t.getUUID().equals(finalUuid)).findFirst().get());
+        }
+        else{
+            container.territoryInfo.permissions.put(uuid,new PermissionFlag());
+            playerList.children().add(playerList.new PlayerEntry(uuid,name,playerList));
+        }
+    }
+
+    private void updateSuggestion()
+    {
+        if(addTextField.getText().length()>0)
+        {
+            Optional<Map.Entry<UUID, String>> first =
+                    UsernameCache.getMap().entrySet().stream().filter(t -> t.getValue().indexOf(addTextField.getText())==0).findFirst();
+            if(first.isPresent())
+            {
+                addTextFieldSuggestion=first.get().getValue().replace(addTextField.getText(),"");
+                addPlayerBtn.active=true;
+            }else{
+                addPlayerBtn.active=false;
+                addTextFieldSuggestion="";
+            }
+        }else{
+            addPlayerBtn.active=false;
+            addTextFieldSuggestion="ADD PLAYER";
+        }
+        addTextField.setSuggestion(addTextFieldSuggestion);
+
+        if(search.getText().length()>0)
+            search.setSuggestion("");
+        else
+            search.setSuggestion("SEARCH");
     }
 }
